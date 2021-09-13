e3090d8202cc3f74cdf2202fcd0a8236b7c0751f
Thource <reyitgex@gmail.com>
2021-09-13 16:57:57 +0100

    Change y rotation to rotate the camera around the character, not the ground
    Refactor commands
    Limit xoffset
    Increase zoom limit (doubled)

--- src/main/java/org/tomgr/clientmods/thirdperson/ThirdPerson.java
+++ src/main/java/org/tomgr/clientmods/thirdperson/ThirdPerson.java
@@ -4,30 +4,29 @@ import com.wurmonline.client.game.World;
 import com.wurmonline.client.renderer.RenderVector;
 import org.gotti.wurmunlimited.modloader.ReflectionUtil;
 import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
-import org.gotti.wurmunlimited.modloader.classhooks.InvocationHandlerFactory;
 import org.gotti.wurmunlimited.modloader.interfaces.Initable;
 import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmClientMod;
 import org.gotti.wurmunlimited.modsupport.console.ConsoleListener;
 import org.gotti.wurmunlimited.modsupport.console.ModConsole;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.util.logging.Logger;
 
 public class ThirdPerson implements WurmClientMod, Initable, PreInitable, ConsoleListener {
     private boolean tpActive = false;
-    private static Logger logger = Logger.getLogger(ThirdPerson.class.getName());
-    private float dist = 4.0f;
-    private float pitch = 45.0f;
-    private float zoomFactor = 1.0f;
-    private float xOffset = 0.0f;
+    private final float dist = 4f;
 
-    private static final float ZOOM_MAX = 2.5f;
-    private static final float ZOOM_MIN = 0.0f;
+    private float zoomFactor = 1f;
 
+    private static final float ZOOM_MAX = 5f;
+    private static final float ZOOM_MIN = 0f;
+
+    private float pitch = 45f;
     private static final float PITCH_MAX = 89.9f;
-    private static final float PITCH_MIN = 30.0f;
+    private static final float PITCH_MIN = 1f;
+
+    private float xOffset = 0f;
+    private static final float X_OFFSET_MAX = 0.1f;
+    private static final float X_OFFSET_MIN = -0.1f;
 
     @Override
     public void preInit() {
@@ -38,120 +37,70 @@ public class ThirdPerson implements WurmClientMod, Initable, PreInitable, Consol
     public void init() {
         HookManager.getInstance().registerHook("com.wurmonline.client.renderer.WorldRender",
                 "calculateCameraOffset", null,
-                new InvocationHandlerFactory() {
-                    @Override
-                    public InvocationHandler createInvocationHandler() {
-                        return new InvocationHandler() {
-                            @Override
-                            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                                method.invoke(proxy, args);
-                                if (tpActive) {
-                                    RenderVector camera = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "cameraOffset"));
-                                    World world = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
+                () -> (proxy, method, args) -> {
+                    method.invoke(proxy, args);
+                    if (tpActive) {
+                        RenderVector cameraOffset = ReflectionUtil.getPrivateField(
+                                proxy, ReflectionUtil.getField(proxy.getClass(), "cameraOffset"));
+                        World world = ReflectionUtil.getPrivateField(
+                                proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
 
-                                    float x = camera.getX();
-                                    float y = camera.getY();
-                                    float z = camera.getZ();
-                                    double yaw = Math.atan2(z, x);
-
-                                    RenderVector target = new RenderVector(xOffset, 0,  0);
-                                    target.rotateY(Math.toRadians(world.getPlayerRotX()));
-                                    camera.add(target);
-                                }
-                                return null;
-                            }
-                        };
+                        RenderVector target = new RenderVector(xOffset, 0, 0);
+                        target.rotateY(Math.toRadians(world.getPlayerRotX()));
+                        cameraOffset.add(target);
                     }
+                    //noinspection SuspiciousInvocationHandlerImplementation
+                    return null;
                 });
 
         HookManager.getInstance().registerHook("com.wurmonline.client.renderer.WorldRender",
                 "getCameraX", null,
-                new InvocationHandlerFactory() {
-                    @Override
-                    public InvocationHandler createInvocationHandler() {
-                        return new InvocationHandler() {
+                () -> (proxy, method, args) -> {
+                    if (!tpActive)
+                        return method.invoke(proxy, args);
 
-                            @Override
-                            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                                if (tpActive) {
-                                    RenderVector camera = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "cameraOffset"));
-                                    World world = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
+                    RenderVector cameraOffset = ReflectionUtil.getPrivateField(
+                            proxy, ReflectionUtil.getField(proxy.getClass(), "cameraOffset"));
+                    World world = ReflectionUtil.getPrivateField(
+                            proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
 
-                                    float x = camera.getX();
-                                    float y = camera.getY();
-                                    float z = camera.getZ();
-                                    double yaw = Math.atan2(z, x);
-                                    setPitch(world.getPlayerRotY());
-                                    return zoomFactor * -dist * Math.cos(yaw) * Math.cos(Math.toRadians(pitch)) + (float) method.invoke(proxy, args);
-                                }
-                                else {
-                                    return method.invoke(proxy, args);
-                                }
-                            }
-                        };
-                    }
+                    float x = cameraOffset.getX();
+                    float z = cameraOffset.getZ();
+                    double yaw = Math.atan2(z, x);
+                    setPitch(world.getPlayerRotY());
+                    return (float) method.invoke(proxy, args) + Math.cos(Math.toRadians(pitch)) * Math.cos(yaw) * zoomFactor * -dist;
                 });
 
         HookManager.getInstance().registerHook("com.wurmonline.client.renderer.WorldRender",
                 "getCameraY", null,
-                new InvocationHandlerFactory() {
-                    @Override
-                    public InvocationHandler createInvocationHandler() {
-                        return new InvocationHandler() {
-                            @Override
-                            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                                if (tpActive) {
-                                    RenderVector camera = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "cameraOffset"));
-                                    World world = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
+                () -> (proxy, method, args) -> {
+                    if (!tpActive)
+                        return method.invoke(proxy, args);
 
-                                    float x = camera.getX();
-                                    float y = camera.getY();
-                                    float z = camera.getZ();
-                                    setPitch(world.getPlayerRotY());
+                    World world = ReflectionUtil.getPrivateField(
+                            proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
 
-                                    return zoomFactor * Math.sin(Math.toRadians(pitch)) + (float) method.invoke(proxy, args);
-                                }
-                                else {
-                                    return method.invoke(proxy, args);
-                                }
-                            }
-                        };
-                    }
+                    setPitch(world.getPlayerRotY());
+
+                    return world.getPlayerPosH() + 1.5f + Math.sin(Math.toRadians(getClampedPitch())) * zoomFactor * dist;
                 });
 
         HookManager.getInstance().registerHook("com.wurmonline.client.renderer.WorldRender",
                 "getCameraZ", null,
-                new InvocationHandlerFactory() {
-                    @Override
-                    public InvocationHandler createInvocationHandler() {
-                        return new InvocationHandler() {
-                            @Override
-                            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                                if (tpActive) {
-                                    RenderVector camera = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "cameraOffset"));
-                                    World world = ReflectionUtil.getPrivateField(
-                                            proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
+                () -> (proxy, method, args) -> {
+                    if (!tpActive)
+                        return method.invoke(proxy, args);
 
-                                    float x = camera.getX();
-                                    float y = camera.getY();
-                                    float z = camera.getZ();
-                                    double yaw = Math.atan2(z, x);
-                                    setPitch(world.getPlayerRotY());
-                                    return zoomFactor * -dist * Math.sin(yaw) * Math.cos(Math.toRadians(pitch)) + (float) method.invoke(proxy, args);
-                                }
-                                else {
-                                    return method.invoke(proxy, args);
-                                }
-                            }
-                        };
-                    }
+                    RenderVector cameraOffset = ReflectionUtil.getPrivateField(
+                            proxy, ReflectionUtil.getField(proxy.getClass(), "cameraOffset"));
+                    World world = ReflectionUtil.getPrivateField(
+                            proxy, ReflectionUtil.getField(proxy.getClass(), "world"));
+
+                    float x = cameraOffset.getX();
+                    float z = cameraOffset.getZ();
+                    double yaw = Math.atan2(z, x);
+                    setPitch(world.getPlayerRotY());
+                    return (float) method.invoke(proxy, args) + Math.sin(yaw) * Math.cos(Math.toRadians(pitch)) * zoomFactor * -dist;
                 });
         ModConsole.addConsoleListener(this);
     }
@@ -161,69 +110,89 @@ public class ThirdPerson implements WurmClientMod, Initable, PreInitable, Consol
     }
 
     public void setZoom(float factor) {
-        zoomFactor = factor;
-
-        if (zoomFactor < ZOOM_MIN) {
-            zoomFactor = ZOOM_MIN;
-        }
-
-        if (zoomFactor > ZOOM_MAX) {
-            zoomFactor = ZOOM_MAX;
-        }
-    }
-
-    public void addPitch(float factor) {
-        setPitch(pitch + factor);
+        zoomFactor = Math.max(ZOOM_MIN, Math.min(factor, ZOOM_MAX));
     }
 
     public void setPitch(float factor) {
         pitch = factor;
-
-        if (pitch < PITCH_MIN) {
-            pitch = PITCH_MIN;
-        }
-
-        if (pitch > PITCH_MAX) {
-            pitch = PITCH_MAX;
-        }
     }
 
-    public void toggleTP() {
-        tpActive = !tpActive;
+    public void setXOffset(float factor) {
+        xOffset = Math.max(X_OFFSET_MIN, Math.min(factor, X_OFFSET_MAX));
+    }
+
+    public float getClampedPitch() {
+        return Math.max(PITCH_MIN, Math.min(pitch, PITCH_MAX));
     }
 
     @Override
     public boolean handleInput(String string, Boolean aBoolean) {
-        if (string != null && string.startsWith("toggle tp")) {
-            toggleTP();
-            return true;
-        }
+        if (string == null) return false;
 
-        if (string != null && string.startsWith("tp zoom-in")) {
-            addZoom(-0.1f);
-            return true;
-        }
+        String[] args = string.split("\\s+");
+        if (!args[0].equals("tp")) return false;
 
-        if (string != null && string.startsWith("tp zoom-out")) {
-            addZoom(0.1f);
-            return true;
-        }
+        if (args.length > 1) {
+            String command = args[1];
+            switch (command) {
+                case "on":
+                case "enable":
+                    tpActive = true;
+                    System.out.println("[ThirdPerson] Enabled");
+                    return true;
+                case "off":
+                case "disable":
+                    tpActive = false;
+                    System.out.println("[ThirdPerson] Disabled");
+                    return true;
+                case "toggle":
+                    tpActive = !tpActive;
+                    System.out.printf("[ThirdPerson] %s%n", tpActive ? "Enabled" : "Disabled");
+                    return true;
+                case "zoom-in":
+                    addZoom(-0.1f);
+                    System.out.println("[ThirdPerson] Zoomed in");
+                    return true;
+                case "zoom-out":
+                    addZoom(0.1f);
+                    System.out.println("[ThirdPerson] Zoomed out");
+                    return true;
+                case "set-zoom":
+                    if (args.length <= 2) {
+                        System.out.println("[ThirdPerson] No zoom specified");
+                        return true;
+                    }
 
-        if (string != null && string.startsWith("tp set-xoffset")) {
-            String[] tokens = string.split("\\s+");
+                    try {
+                        setZoom(Float.parseFloat(args[2]));
+                        System.out.printf("[ThirdPerson] Zoom set to %f%n", zoomFactor);
+                    } catch (NumberFormatException e) {
+                        System.out.println("[ThirdPerson] Zoom must be a number");
+                    }
+                    return true;
+                case "get-zoom":
+                    System.out.printf("[ThirdPerson] Zoom = %f%n", zoomFactor);
+                    return true;
+                case "set-xoffset":
+                    if (args.length <= 2) {
+                        System.out.println("[ThirdPerson] No offset specified");
+                        return true;
+                    }
 
-            if (tokens.length != 3) {
-                System.out.println("Format: tp set-xoffset <value>");
+                    try {
+                        setXOffset(Float.parseFloat(args[2]));
+                        System.out.printf("[ThirdPerson] XOffset set to %f%n", xOffset);
+                    } catch (NumberFormatException e) {
+                        System.out.println("[ThirdPerson] Offset must be a number");
+                    }
+                    return true;
+                case "get-xoffset":
+                    System.out.printf("[ThirdPerson] XOffset = %f%n", xOffset);
+                    return true;
             }
-
-            try {
-                xOffset = Float.parseFloat(tokens[2]);
-            }
-            catch (NumberFormatException e) {
-                System.out.println("Format: tp set-xoffset <value>");
-            }
-            return true;
         }
-        return false;
+
+        System.out.println("[ThirdPerson] Valid commands are: enable/on, disable/off, toggle, zoom-in, zoom-out, set-zoom ZOOM, get-zoom, set-xoffset OFFSET, get-xoffset");
+        return true;
     }
 }
